35809ee7412fbca880228efef362da8baa178720
mehdi <elmahboubimehdi@gmail.com>
2026-09-08 21:58:02 +0100

    Always show country dropdown in checkout

--- src/lib/shipping.ts
+++ src/lib/shipping.ts
@@ -419,7 +419,7 @@ export function isPaypalCheckoutFlow(checkoutFlow?: string | null): boolean {
 }
 
 export function usesCountryFirstAddress(checkoutFlow?: string | null): boolean {
-  return checkoutFlow === 'kofi' || isPaypalCheckoutFlow(checkoutFlow);
+  return true;
 }
 
 export function getCountryName(countryCode: string): string {
